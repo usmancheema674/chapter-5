0fa83d30bf1120f26ea9d5eb4fe0ced4f99a8b53
usmancheema674 <58159427+usmancheema674@users.noreply.github.com>
2019-12-23 00:31:46 -0800

    Add files via upload

---
+++ 5.25.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+main()
+{
+for(int i =1; i<=100; i++){
+	
+
+	if(i == 10){
+	continue;
+	
+	}
+	else{
+		cout<<i<<"\n";
+	}
+	
+	
+	
+	
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+}
